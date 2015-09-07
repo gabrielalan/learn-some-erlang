a4987d24ff68737e192b34a3c22d81bec5f79e73
Gabriel Alan <gaba.alan@gmail.com>
2015-09-07 12:16:41 -0300

    List comprehension

--- useless.erl
+++ useless.erl
@@ -1,7 +1,9 @@
 %% Learning erlang modules
-
+%% Module declaration
 -module(useless).
 
+%% Choose what functions will be exported
+%% The notation name/2 is = function name with 2 parameters and all functions have this signature
 -export([add/2, add/3, hello/0, greet_and_add_two/1]).
 
 add(A, B) ->
@@ -18,3 +20,10 @@ hello() ->
 greet_and_add_two(X) ->
 	hello(),
 	add(X, 2).
+	
+get_list_comprehension() ->
+	[X || X <- [1,2,3,4,5,6,7,8,9,10], X rem 2 =:= 0].
+	
+tuple_list_comprehension() ->
+	RestaurantMenu = [{steak, 5.99}, {beer, 3.99}, {poutine, 3.50}, {kitten, 20.99}, {water, 0.00}].
+	[{Item, Price*1.07} || {Item, Price} <- RestaurantMenu, Price >= 3, Price =< 10].
